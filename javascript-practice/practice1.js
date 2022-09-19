21edbc987950bf11c6775666c10c7d3f9b3fe8c3
ameliaamal <hk.amal221@gmail.com>
2022-09-19 08:20:11 -0700

    some updates

---
+++ javascript-practice/practice1.js
@@ -0,0 +1,22 @@
+console.log("hello world");
+
+let myname = "amal";
+let favcolor = "gray";
+let myage = 29;
+const datebirth = "Feb 7, 1993";
+
+console.log("my name is "+ myname + " and I have " + myage + " old, I am born on" + datebirth + "and  my favorite color is" + favcolor);
+
+let myFname= "rania";
+let myFage = 32;
+let myFfcolor = "black";
+const fdatebirth = "jan 21, 1990";
+if (myFage > myage) {
+    console.log(myFname + " is older than " + myname);
+} 
+
+myage = myage + 1;
+console.log("my name is "+ myname + " and I have " + myage + " old, I am born on" + datebirth + "and  my favorite color is" + favcolor);
+
+let sum = myFage + myage;
+ console.log("sum of amal and rania is " + sum);
